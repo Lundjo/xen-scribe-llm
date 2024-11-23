d08c31ac70d4ebe03e7672fed5dba978f66c9035
Danijel Jovanović <danijel.xda@gmail.com>
2024-11-23 17:05:58 +0100

    Prepare for information copy

--- src/components/info/Information.jsx
+++ src/components/info/Information.jsx
@@ -4,15 +4,54 @@ import Transcription from "../transcribe/Transcription";
 export default function Information(props) {
   const { output, finished } = props;
   const [tab, setTab] = useState("transcription");
-  const [translation, setTranslation] = useState(null);
   const [translating, setTranslating] = useState(null);
 
   const worker = useRef();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("../utils/translate.worker.js", import.meta.url),
+        { type: "module" }
+      );
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.status) {
+        case "initiate":
+          break;
+        case "progress":
+          break;
+        case "update":
+          setTranslation(e.data.output);
+          break;
+        case "complete":
+          setTranslating(false);
+          break;
+      }
+    };
+
+    worker.current.addEventListener("message", onMessageReceived);
+
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
+  }, []);
 
   const textElement =
-    tab === "transcription" ? output.map((val) => val.text) : translation || "";
+    tab === "transcription" ? output.map((val) => val.text) : "";
+
+  function handleCopy() {
+    navigator.clipboard.writeText(textElement);
+  }
+
+  function handleDownload() {
+    const element = document.createElement("a");
+    const file = new Blob([textElement], { type: "text/plain" });
+    element.href = URL.createObjectURL(file);
+    element.download = `transcribed_${Date.now().toString()}.txt`;
+    document.body.appendChild(element);
+    element.click();
+  }
 
   return (
     <main className="flex flex-col items-center p-8 gap-8 text-center max-w-prose w-full mx-auto bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-lg rounded-xl shadow-lg">
@@ -39,11 +78,24 @@ export default function Information(props) {
             <i className="fa-solid fa-spinner text-primary-400 text-4xl"></i>
           </div>
         )}
-        {tab === "transcription" ? (
-          <Transcription {...props} textElement={textElement} />
-        ) : (
-          <></>
-        )}
+        <Transcription {...props} textElement={textElement} />
+      </div>
+
+      <div className="flex gap-4 justify-center">
+        <button
+          onClick={handleCopy}
+          title="Copy"
+          className="py-2 px-4 bg-white/10 rounded-xl hover:bg-primary-600 transition text-primary-400 hover:text-white shadow-lg transform hover:scale-105"
+        >
+          <i className="fa-solid fa-copy"></i>
+        </button>
+        <button
+          onClick={handleDownload}
+          title="Download"
+          className="py-2 px-4 bg-white/10 rounded-xl hover:bg-primary-600 transition text-primary-400 hover:text-white shadow-lg transform hover:scale-105"
+        >
+          <i className="fa-solid fa-download"></i>
+        </button>
       </div>
     </main>
   );
